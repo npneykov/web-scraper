4e19762d24b336fffdcdf967c3ecc81fc6a5728d
npneykov <nikola.neykov@gmail.com>
2024-02-07 14:19:52 +0200

    Fixed main.py naming, added pre-commit hooks

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,10 @@
 from scraper import run_web_scraper, write_data_to_file
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Example usage
     print(
         write_data_to_file(
-            "website_data.json",
-            run_web_scraper("https://example.com", ["div", "p", "a"]),
+            'website_data.json',
+            run_web_scraper('https://example.com', ['div', 'p', 'a']),
         )
     )
